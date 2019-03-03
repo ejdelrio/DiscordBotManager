35e115a691cdf62398f762eb351bf1b4a10a65fd
Eddie <Eddie@Edwins-MBP.hitronhub.home>
2019-03-03 12:54:06 -0800

    Added additional file scaffolding

--- main.js
+++ main.js
@@ -5,12 +5,8 @@ const debug = require("debug")("DiscordBotManager : main.js");
 const PORT = process.env.PORT || 4000;
 const app = new express();
 
-function example(a, b, c)
-{
-
-}
-
 app.listen(PORT, () =>
 {
-  debug("SERVER ACTIVE ON PORT : ", PORT);
+  let log = debug.extend("app.listen");
+  log("SERVER ACTIVE ON PORT : ", PORT);
 }); 
