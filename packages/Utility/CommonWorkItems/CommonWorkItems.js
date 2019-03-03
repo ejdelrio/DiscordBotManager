60503af414845b69860bebeae5e40d9c09651ef8
Eddie <Eddie@Edwins-MBP.hitronhub.home>
2019-03-03 13:20:35 -0800

    Added methods to CommonWorkItems.js

---
+++ packages/Utility/CommonWorkItems/CommonWorkItems.js
@@ -0,0 +1,86 @@
+"use strict";
+
+const debug = require("debug")("DiscordBotManager");
+
+class CommonWorkItems
+{
+  static IsNullOrWhiteSpace(str)
+  {
+    var log = debug.extend(CommonWorkItems.IsNullOrWhiteSpace.name);
+    log("Beginning");
+
+    if (CommonWorkItems.IsNullOrEmpty(str))
+    {
+      return true;
+    }
+
+    for (let i = 0; i < str.length; i++)
+    {
+      if (str[ i ] != " ")
+      {
+        return false;
+      }
+    }
+
+    return true;
+  }
+  static IsNullOrEmpty(str)
+  {
+    var log = debug.extend(CommonWorkItems.IsNullOrEmpty.name);
+    log("Begining");
+
+    if (str === null || str.length === 0)
+    {
+      log("Exiting");
+      return true;
+    }
+
+    log("Exiting");
+    return false;
+  }
+
+  static RemovePadding(str)
+  {
+    var log;
+    var formattedString;
+    var startOfString;
+    var endOfString;
+
+    if (CommonWorkItems.IsNullOrEmpty(str))
+    {
+      return "";
+    }
+
+    log = debug.extend(CommonWorkItems.RemovePadding.name);
+    log("Beginning");
+    log.arguments(str);
+    formattedString = new String();
+
+    for (startOfString = 0; i < str.length; startOfString++)
+    {
+      if (str[ startOfString ] != " ")
+      {
+        break;
+      }
+    }
+
+    for (endOfString = str.length - 1; endOfString >= 0; endOfString--)
+    {
+      if (str[ endOfString ] != " ")
+      {
+        break;
+      }
+    }
+
+    while (startOfString <= endOfString)
+    {
+      formattedString += str[ startOfString ];
+      startOfString++;
+    }
+
+    log("Ending");
+    return formattedString;
+  }
+}
+
+modules.exports = CommonWorkItems;
